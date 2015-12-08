1e87795a3525394826f6a58969b58f2311993ed7
rainik <itrainik@gmail.com>
2015-12-08 22:39:50 +0200

    Added test page
    Добавлена страница теста с кодом

--- test.js
+++ test.js
@@ -1,37 +1,40 @@
- var test = document.forms.quiz;
- var linksQ = test.elements.links;
- var listQ = test.elements.list;
- var servQ = test.elements.serv;
- var checkB = document.getElementsByName("box");
- var checkB1 = document.getElementsByName("box1");
-
+var firstArr = $(":radio");
+var firstBoxes = $(":checkbox.box");
+var lastBoxes = $(":checkbox.box1");
 var results = 0;
 var tmpRes = 0;
 
-function showResults() {
-	if (linksQ[0].checked) {
+$("#sendform").bind("click", function() {
+	for (var i = 0; i < firstArr.length; i++) {
+		if (i ==0 && firstArr.eq(i).prop("checked") || i == 6 && firstArr.eq(i).prop("checked") || i == 11 && firstArr.eq(i).prop("checked")) {
+			results += 2;
+		}
+	}
+	for (var i = 0; i < firstBoxes.length; i++) {
+		if (firstBoxes.eq(i).prop("checked")) {
+			if (i == 1 || i == 3) {
+				tmpRes += 1;
+			} else {
+				tmpRes -= 1;
+			}
+		}
+	}
+	if ( tmpRes == 2 ) {
 		results += 2;
-	};
-	if (listQ[2].checked) {
-		results += 2;
-	};
-	if (servQ[2].checked) {
-		results += 2;
-	};
-
-	if  (checkB[0].checked) {
-	 tmpRes -= 1;
+		tmpRes = 0;
+	} else {
+		results += 0;
+		tmpRes = 0;
 	}
-	if  (checkB[1].checked) {
-	 tmpRes += 1;
+	for (var i = 0; i < lastBoxes.length; i++) {
+		if (lastBoxes.eq(i).prop("checked")) {
+			if (i == 0 || i == 1) {
+				tmpRes += 1;
+			} else {
+				tmpRes -= 1;
+			}
+		}
 	}
-	if  (checkB[2].checked) {
-	 tmpRes -= 1;
-	}
-	if  (checkB[3].checked) {
-	 tmpRes += 1;
-	}
-	
 	if ( tmpRes == 2 ) {
 		results += 2;
 		tmpRes = 0;
@@ -40,28 +43,8 @@ function showResults() {
 		tmpRes = 0;
 	}
 	
-	if  (checkB1[0].checked) {
-	 tmpRes += 1;
-	}
-	if  (checkB1[1].checked) {
-	 tmpRes += 1;
-	}
-	if  (checkB1[2].checked) {
-	 tmpRes -= 1;
-	}
-	if  (checkB1[3].checked) {
-	 tmpRes -= 1;
-	}
-	
-	if ( tmpRes == 2 ) {
-		results += 2;
-	} else {
-		results += 0;
-	}
-
-//return results;
-alert("You've obtained " + results + " points as you gave " + results * 10 + "% right answers"); 
-}
-
-
-
+	alert("You've obtained " + results + " points as you gave " + results * 10 + "% right answers");
+	tmpRes = 0;
+	results = 0;
+	location.reload();
+});
